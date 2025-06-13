6db9ceebca6ad448fd9cde43ed7ede84e238b09c
dev05 <dev05@dev.co-mit.com>
2025-06-13 16:27:11 +0700

    front-end component navbar

--- front-end/src/components/user/Navbar/index.jsx
+++ front-end/src/components/user/Navbar/index.jsx
@@ -1,8 +1,21 @@
+import { FaHome } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={styles['menu-top']}>
+      <ul className={styles.menu}>
+        <li>
+          <Link><FaHome /></Link>
+        </li>
+        <li>
+          <Link>HOT</Link>
+        </li>
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
     </div>
   )
 }
