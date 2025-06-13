06d5520f62efe063f350516176f23f201387034e
dev05 <dev05@dev.co-mit.com>
2025-06-13 16:27:11 +0700

    front-end component navbar

--- front-end/src/components/user/Navbar/index.jsx
+++ front-end/src/components/user/Navbar/index.jsx
@@ -1,8 +1,31 @@
+import { FaFire, FaHistory, FaHome, FaTags } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={styles['menu-top']}>
+      <ul className={styles.menu}>
+        <li>
+          <Link className={styles['menu-action']}><FaHome /> HOME</Link>
+        </li>
+        <li>
+          <Link className={styles['menu-action']}><FaFire /> HOT</Link>
+        </li>
+        <li>
+          <Link className={styles['menu-action']}><FaTags /> THỂ LOẠI <FaCaretDown /></Link>
+        </li>
+        <li>
+          <Link className={styles['menu-action']}>LỊCH SỬ <FaHistory /></Link>
+        </li>
+        <li>
+          <Link className={styles['menu-action']}>THEO DÕI</Link>
+        </li>
+        <li>
+          <Link className={styles['menu-action']}>XẾP HẠNG <FaCaretDown /></Link>
+        </li>
+      </ul>
     </div>
   )
 }
